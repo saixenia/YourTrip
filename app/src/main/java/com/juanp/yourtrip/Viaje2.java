b8ff1c5c1f338d0f0ec168aa6208c9c3fc2796ba
Julian Rodriguez <julian-rodriguez92@hotmail.com>
2018-04-23 21:02:32 -0500

    cambio de layouts

---
+++ app/src/main/java/com/juanp/yourtrip/Viaje2.java
@@ -0,0 +1,14 @@
+package com.juanp.yourtrip;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Viaje2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viaje2);
+    }
+}
+///hjhjkkjkhjjkh
