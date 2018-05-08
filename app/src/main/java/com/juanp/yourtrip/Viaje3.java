e8ffba1fffe0530989f0a1931b45b8067c2b7d1a
Julian Rodriguez <julian-rodriguez92@hotmail.com>
2018-05-07 19:37:37 -0500

    cambio de colores y viaje 3

---
+++ app/src/main/java/com/juanp/yourtrip/Viaje3.java
@@ -0,0 +1,13 @@
+package com.juanp.yourtrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Viaje3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viaje3);
+    }
+}
