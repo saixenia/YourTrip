56291f109576f32b2cbb5e27fb9ff8433dab47dd
Julian Rodriguez <julian-rodriguez92@hotmail.com>
2018-05-21 19:32:50 -0500

    Perfil

--- app/src/main/java/com/juanp/yourtrip/Login.java
+++ app/src/main/java/com/juanp/yourtrip/Login.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.juanp.yourtrip.Perfil;
 
 /*sharedpreferences para las sesiones
 * */
